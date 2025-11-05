3d6cebc5206b9a6fcb04560c65955327a6587576
epinki07 <dramirezmagana@gmail.com>
2025-11-05 08:36:21 -0600

    Ejercicio 8 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -129,8 +129,17 @@ function mayorDeTres(a, b, c) {
  */
 function clasificarEdad(edad) {
   // TODO: Implementa la clasificaciÃ³n con if/else if/else
-  return "";
-}
+    if (edad >= 0 && edad <= 17) {
+    return "menor";
+  } else if (edad >= 18 && edad <= 64) {
+    return "adulto";
+  } else if (edad >= 65) {
+    return "mayor";
+  } 
+  }
+
+  
+
 
 // ============================================
 // SECCIÃ“N 3: FUNCIONES Y BUCLES (20 puntos)
